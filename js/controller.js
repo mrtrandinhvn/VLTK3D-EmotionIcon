edfed11d680ce0e02a2954708422fd1f2badd063
goseidinh <tran.dinh@gosei.com.vn>
2015-06-29 09:26:44 +0700

    v1.4 : add emotions

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,5 @@
 function Controller() {
+    // for global use
     this.EmotionList = [
         "ac",
         "biumoi",
@@ -55,9 +56,10 @@ function Controller() {
 		"bia",
 		"chamthan",
 		"chuoi",
-		"win",
-	];
-
+		"win"
+    ];
+    // for local use
+    var emotionList = this.EmotionList;
 
     this.onLoaded = function () {
         var loadedChatWindows = new ChatWindows();
@@ -66,5 +68,43 @@ function Controller() {
     this.onRunTime = function (changedElement) {
         var workingChatWindow = new ChatWindows();
         return workingChatWindow.load(changedElement);
+    }
+    // public function
+    this.editSingleRow = function (rows) {
+        for (var j = 0; j < rows.length; j++) {
+            var replacedHtml = replaceHtml(rows[j]);
+            if (replacedHtml) {
+                $(rows[j]).html(replacedHtml);
+            }
+        }
+    }
+
+    // private functions
+    function replaceHtml(rowSelector) {
+        var outputHtml = $(rowSelector).html();
+        if (outputHtml.includes("#")) { // check hashtag
+            for (var j = 0; j < emotionList.length; j++) {
+                var emoHtml = "<span class='emoticon emoticon_" + emotionList[j] + "'></span>";
+                // check contain right emotion tag
+                var emoTag = "#" + emotionList[j];
+                var afterHasTagWord = outputHtml.indexOf(emoTag) + emotionList[j].length + 1; // +1 for '#'
+                var regex = new RegExp("[a-zA-Z0-9]");
+                if (
+                    outputHtml.includes(emoTag + " ")
+                        || !outputHtml.charAt(afterHasTagWord).match(regex)
+                        || outputHtml.length == afterHasTagWord
+                ) {
+                    // replace text with image
+                    outputHtml = replaceAll(outputHtml, emoTag, emoHtml);
+                }
+            }
+            return outputHtml;
+        }
+        return null;
+    }
+
+    // replace all 'find' = 'replace' in 'inputStr'
+    function replaceAll(inputStr, find, replace) {
+        return inputStr.replace(new RegExp(find, 'g'), replace);
     }
 }
--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     if (result1) {
         if (result1.chatRow.length) {
             for (var i = 0; i < result1.chatRow.length; i++) {
-                editSingleRow(result1.chatRow[i]);
+                controller.editSingleRow(result1.chatRow[i]);
             }
         }
     }
@@ -15,49 +15,9 @@ $(document).ready(function () {
         if (result2) {
             if (result2.chatRow.length) {
                 for (var m = 0; m < result2.chatRow.length; m++) {
-                    editSingleRow(result2.chatRow[m]);
+                    controller.editSingleRow(result2.chatRow[m]);
                 }
             }
         }
     });
-
-    // private functions
-    function editSingleRow(rows) {
-        for (var j = 0; j < rows.length; j++) {
-            var replacedHtml = replaceHtml(rows[j]);
-            if (replacedHtml) {
-                $(rows[j]).html(replacedHtml);
-            }
-        }
-    }
-
-    function replaceHtml(rowSelector) {
-
-        var outputHtml = $(rowSelector).html();
-        var emotionList = controller.EmotionList;
-        if (outputHtml.includes("#")) { // check hashtag
-            for (var j = 0; j < emotionList.length; j++) {
-                var emoHtml = "<span class='emoticon emoticon_" + emotionList[j] + "'></span>";
-                // check contain right emotion tag
-                var emoTag = "#" + emotionList[j];
-                var afterHasTagWord = outputHtml.indexOf(emoTag) + emotionList[j].length + 1; // +1 for '#'
-                var regex = new RegExp("[a-zA-Z0-9]");
-                if (
-                    outputHtml.includes(emoTag + " ")
-                        || !outputHtml.charAt(afterHasTagWord).match(regex)
-                        || outputHtml.length == afterHasTagWord
-                ) {
-                    // replace text with image
-                    outputHtml = replaceAll(outputHtml, emoTag, emoHtml);
-                }
-            }
-            return outputHtml;
-        }
-        return null;
-    }
-
-    // replace all 'find' = 'replace' in 'inputStr'
-    function replaceAll(inputStr, find, replace) {
-        return inputStr.replace(new RegExp(find, 'g'), replace);
-    }
 });
