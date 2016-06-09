473559b554758e3d75b0426e5d5cf1c28a327930
goseivietnam <goseidinh>
2016-06-09 10:19:08 +0700

    Add changelog into popup

--- js/popup/controller.js
+++ js/popup/controller.js
@@ -1,6 +1,8 @@
-function Controller() {
+﻿function Controller() {
     this.initialize = function (selector) {
-        document.querySelector(selector).innerHTML += ("<div>Current version: " + appManifest.version + "</div>");
+        document.querySelector(selector).innerHTML += ("<div>Phiên bản: " + appManifest.version + "</div>");
+        document.querySelector(selector).innerHTML += ("<div>- Đổi biểu tượng #choang và #hon cho chuẩn.</div>");
+        document.querySelector(selector).innerHTML += ("<div>- Thêm một vài biểu tượng mới.</div>");
         for (var i = 0; i < emotionList.length; i++) {
             var oneRowHtml = "<div class='one-row'><span class='emoticon emoticon_" + emotionList[i] + "'></span><input class='text' disabled value=\"" + "#" + emotionList[i] + "\"></div>";
             document.querySelector(selector).innerHTML += oneRowHtml;
